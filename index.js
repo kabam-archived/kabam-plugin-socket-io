9b14c5ceeff6962ff824750906a6e2855eb58ed8
Muhammad Ghazali <muhammadghazali2480@gmail.com>
2013-10-03 03:05:54 +0700

    style(index.js): reindent the source code

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var http = require('http'),
 
 exports.name = 'kabam_socket';
 
-exports.app = function (kabam) {
+exports.app = function(kabam) {
   server = http.createServer(kabam.app);
   var io = ioServer.listen(server);
 
@@ -17,19 +17,19 @@ exports.app = function (kabam) {
   io.enable('browser client etag');
   io.enable('browser client minification');
   io.set('browser client expires', (24 * 60 * 60));
-//*/
-//for heroku or Pound reverse proxy
+  //*/
+  //for heroku or Pound reverse proxy
   io.set('transports', ['xhr-polling']);
   io.set('polling duration', 6);
-//*/
+  //*/
 
 
   //make redis not so verbose
-  kabam.app.configure('production', function () {
+  kabam.app.configure('production', function() {
     io.set('log level', 1);
   });
 
-//*/
+  //*/
   //set io for production - with 3 redis clients
   var ioRedisStore = require('socket.io/lib/stores/redis');
 
@@ -39,77 +39,80 @@ exports.app = function (kabam) {
     redisSub: kabam.createRedisClient(), //it works in sub mode, it cannot access database
     redisClient: kabam.redisClient
   }));
-//*/
-  var sessionStorage = new RedisStore({prefix: 'kabam_sess_', client: kabam.redisClient});
+  //*/
+  var sessionStorage = new RedisStore({
+    prefix: 'kabam_sess_',
+    client: kabam.redisClient
+  });
   io.set("authorization", passportSocketIo.authorize({
-      cookieParser: express.cookieParser,
-      secret: kabam.config.secret,
-      store: sessionStorage,
-      fail: function (data, accept) {    //there is no passportJS user present for this session!
-//        console.log('vvv fail');
-//        console.log(data);
-//        console.log('^^^ fail');
-        data.user = null;
-        accept(null, true);
-      },
-      success: function (data, accept) { //the passportJS user is present for this session!
-//        console.log('vvv success');
-//        console.log(data);
-//        console.log('^^^ success');
+    cookieParser: express.cookieParser,
+    secret: kabam.config.secret,
+    store: sessionStorage,
+    fail: function(data, accept) { //there is no passportJS user present for this session!
+      //        console.log('vvv fail');
+      //        console.log(data);
+      //        console.log('^^^ fail');
+      data.user = null;
+      accept(null, true);
+    },
+    success: function(data, accept) { //the passportJS user is present for this session!
+      //        console.log('vvv success');
+      //        console.log(data);
+      //        console.log('^^^ success');
 
-        sessionStorage.get(data.sessionID, function (err, session) {
-//          console.log('v session');
-//          console.log(session);
-//          console.log('^ session');
-          kabam.model.Users.findOneByApiKey(session.passport.user, function (err, user) {
-            if(user){
-//            console.log('user found '+user.username);
-              data.user = user;
-              accept(err, true);
-            } else {
-              accept(err, false); //we break the session, because someone tryes to tamper it)
-            }
-          });
+      sessionStorage.get(data.sessionID, function(err, session) {
+        //          console.log('v session');
+        //          console.log(session);
+        //          console.log('^ session');
+        kabam.model.Users.findOneByApiKey(session.passport.user, function(err, user) {
+          if (user) {
+            //            console.log('user found '+user.username);
+            data.user = user;
+            accept(err, true);
+          } else {
+            accept(err, false); //we break the session, because someone tryes to tamper it)
+          }
         });
-      }
+      });
     }
-  ));
+  }));
 
-//  io.sockets.on("connection", function (socket) {
-//    console.log("user connected: ");
-//    console.log(socket.handshake);
-//  });
+  //  io.sockets.on("connection", function (socket) {
+  //    console.log("user connected: ");
+  //    console.log(socket.handshake);
+  //  });
 
 
   //emit event to all connected and authorized users
-  kabam.on('broadcast', function (message) {
+  kabam.on('broadcast', function(message) {
     io.sockets.emit('broadcast', message);
   });
 
-//*/
+  //*/
   //catch event created by User.notify() to the user needed only
-  kabam.on('notify:sio', function (message) {
-      var activeUsers = io.sockets.manager.handshaken;
-      for (var x in activeUsers) {
-        if (activeUsers[x].user.username === message.user.username) {
-//          console.log('We can send notify to active user of '+message.user.username);
-//          console.log(io.sockets.manager.sockets.sockets[x]);
-          if (io.sockets.manager.sockets.sockets[x]) {
-            io.sockets.manager.sockets.sockets[x].emit('notify', {'user': message.user, 'message': message.message});
-          }
+  kabam.on('notify:sio', function(message) {
+    var activeUsers = io.sockets.manager.handshaken;
+    for (var x in activeUsers) {
+      if (activeUsers[x].user.username === message.user.username) {
+        //          console.log('We can send notify to active user of '+message.user.username);
+        //          console.log(io.sockets.manager.sockets.sockets[x]);
+        if (io.sockets.manager.sockets.sockets[x]) {
+          io.sockets.manager.sockets.sockets[x].emit('notify', {
+            'user': message.user,
+            'message': message.message
+          });
         }
       }
+    }
   });
 
-  kabam.socket.io=io;
+  kabam.socket.io = io;
 };
 
 exports.core = {
-  'listenWithSocketIo':function(config){
+  'listenWithSocketIo': function(config) {
     return function(port) {
       server.listen(port);
     };
   }
 };
-
-
